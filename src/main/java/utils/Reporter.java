7eebbe0cfad1125bc84661fd9f3d68768315f554
rajguttula <Raju@Raju-PC>
2019-04-23 09:36:05 +0530

    POM Files

---
+++ src/main/java/utils/Reporter.java
@@ -0,0 +1,69 @@
+package utils;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+public class Reporter extends ExtentReports{
+	
+	public static ExtentHtmlReporter reporter;
+	public static ExtentReports extent;
+	public static ExtentTest test;
+	
+	public String testcaseName, testcaseDec, author, category;
+	public String excelSheetName;
+	public static int intLeadID;
+	
+	@BeforeSuite
+	public void startReport() {
+		reporter = new ExtentHtmlReporter("./reports/result.html");
+		reporter.setAppendExisting(true); 
+		extent   = new ExtentReports();
+		extent.attachReporter(reporter);
+	}
+	
+    @BeforeClass
+	public void report() throws IOException {
+		test = extent.createTest(testcaseName, testcaseDec);
+	    test.assignAuthor(author);
+	    test.assignCategory(category);  
+	}
+    
+    public void reportStep(String dec, String status) {
+    	if(status.equalsIgnoreCase("pass")) {
+    		test.pass(dec);
+    	} else if(status.equalsIgnoreCase("fail")) {
+    		test.fail(dec); 
+    	}
+    }
+    
+    @AfterSuite
+    public void stopReport() {
+    	extent.flush();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
